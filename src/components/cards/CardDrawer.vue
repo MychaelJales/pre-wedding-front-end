20a71395ac2559a5c2e1430cb11ed9ffae4d50ef
mychaeljales <mychael_duarte@hotmail.com>
2024-03-26 18:39:53 -0300

    feat: cria componente de card para ser usado dentro do drawer

---
+++ src/components/cards/CardDrawer.vue
@@ -0,0 +1,64 @@
+<script>
+import { computed } from 'vue'
+import { formatMoney } from '@/utils/moneyFormat';
+export default {
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+      // Use computed para formatar o valor automaticamente quando o valor original mudar
+      const formattedValue = computed(() => {
+        return formatMoney(props.product?.price);
+      });
+  
+      return {
+        formattedValue,
+      };
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="product-card">
+      <div class="card-content">
+        <el-space direction="horizontal">
+          <el-text class="mx-1 product-name " >{{ product.name }}</el-text>
+          <el-text class="mx-1 product-name" type="success" size="large">{{ formattedValue }}</el-text>
+        </el-space>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.product-card {
+  max-height: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.card-content {
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+  /* align-content: center; */
+  justify-content: center;
+  margin-right: 8px;
+}
+.product-name {
+  margin: 0;
+  font-weight: bold;
+}
+
+</style>

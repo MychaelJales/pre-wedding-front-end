740dd2f603b21695d1979d8bf024aa491b3b9188
mychaeljales <mychael_duarte@hotmail.com>
2024-03-27 15:17:06 -0300

    feat: cria componente footer

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <el-footer class="my-footer">
+    <el-text>Powered by <a href="https://www.linkedin.com/in/mychael-jales-duarte/" target="_blank">Mychael Jales</a></el-text>
+  </el-footer>
+</template>
+
+<style scoped>
+.my-footer {
+  width: 100%;
+  text-align: center;
+  padding-top: 15px;
+}
+</style>
